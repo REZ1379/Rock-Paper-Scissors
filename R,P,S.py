961b4ccd591b62bad23a98ab7ab4a578b61077cb
Reza <srezarafiee7900@gmail.com>
2022-03-02 14:59:55 +0330

    Update Names

--- R,P,S.py
+++ R,P,S.py
@@ -2,7 +2,7 @@ import random
 print("\nCreated by \"Reza Rafiee\".05-Feb-22")
 print("""This is a \"Hand Game\" called \"Rock,Paper,Scissors\"
 Allowed movements : \n\"Rock\"\n\"Paper\"\n\"Scissors\"
-Whoever \"beats the opponent for Three Times\",Wins.""")
+Whoever \"beats the opponent for 3 Times\",Wins.""")
 RandomNumber = random.randint(0, 2)
 if RandomNumber == 0:
     ComputerMove = "Rock"
@@ -10,17 +10,20 @@ elif RandomNumber == 1:
     ComputerMove = "Paper"
 elif RandomNumber == 2:
     ComputerMove = "Scissors"
+Player_1_Name = input("\nEnter your name please : ").capitalize()
+Player_2_Name = "System"
 Answer = "Y"
 while Answer == "Y":
     cp1 = 0
     cp2 = 0
-    Go = input("\nIf you are ready,Type \"Go\" :\n").capitalize()
+    Go = input(f"""\nHey {Player_1_Name}
+If you are ready,Type \"Go\" :\n""").capitalize()
     while Go != "Go":
         Go = input("Type \"Go\" please :\n").capitalize()
     while cp1 or cp2 != 3:
-        print(f"\nPlayer_1 : {cp1}   |   Player_2 : {cp2}")
-        Player_1 = input("Player_1 , Make your move : ").capitalize()
-        print(f"Player_2 , Make your move : {ComputerMove}")
+        print(f"\n{Player_1_Name} : {cp1}   |   {Player_2_Name} : {cp2}")
+        Player_1 = input(f"{Player_1_Name} , Make your move : ").capitalize()
+        print(f"{Player_2_Name} , Make your move : {ComputerMove}")
         Player_2 = ComputerMove
         if Player_1 == Player_2:
             print("DRAW...!!!\nTry again")
@@ -42,12 +45,14 @@ while Answer == "Y":
         else:
             print("You didn't make an \"ALLOWED\" movement!!!\nTry again")
         if cp1 == 3:
-            print(f"\nFinal scores : Player_1 : {cp1}   |   Player_2 : {cp2}")
-            print("\nPlayer_1 Wins...!!!")
+            print(f"""\nFinal scores :
+{Player_1_Name} : {cp1}   |   {Player_2_Name} : {cp2}""")
+            print(f"\n{Player_1_Name} Wins...!!!")
             break
         elif cp2 == 3:
-            print(f"\nFinal scores : Player_1 : {cp1}   |   Player_2 : {cp2}")
-            print("\nPlayer_2 Wins...!!!")
+            print(f"""\nFinal scores :
+{Player_1_Name} : {cp1}   |   {Player_2_Name} : {cp2}""")
+            print(f"\n{Player_2_Name} Wins...!!!")
             break
     print("\nThanks for playing \U0001f60a\u2764\ufe0f")
     print("-----------------------")
